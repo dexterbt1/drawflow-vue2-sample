a71188136ad81f009d504c1d45479567dab5e601
Dexter Tad-y <dtady@cpan.org>
2023-10-26 16:15:09 +0800

    working add node + style overrides

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app">
-	  <header class="top-toolbar">
+	  <nav class="top-toolbar">
 		<select v-model="selectedNodeType">
             <option value="">-- select --</option>
             <option value="trigger">Trigger</option>
@@ -12,7 +12,7 @@
 			<button @click="exportDF">Export</button>
 			<button @click="importDF">Import</button>
 		</div>
-	  </header>
+	  </nav>
 	  <div id="drawflow"></div>
 	</div>
 </template>
@@ -23,11 +23,11 @@
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
   }
-  header.top-toolbar {
+  nav.top-toolbar {
 	padding: 10px;
 	box-shadow: 3px 5px 20px #8883;
   }
-  header.top-toolbar select {
+  nav.top-toolbar select {
 	width: 15rem;
 	margin: 0 7px 0 3px;
   }
@@ -46,27 +46,46 @@
   
 
 <script>
+
+  // FIXME: note this is a monolith example - refactor and design this in their proper classes, components, etc 
+
+
   /*eslint-disable */
+
+  import Vue from 'vue'
+
   import Drawflow from 'drawflow'
   import styleDrawflow from 'drawflow/dist/drawflow.min.css'
 
   import TriggerOnly from './components/nodes/TriggerOnly.vue'
   import HelloWorld from './components/HelloWorld.vue'
   import HelloWorld2 from './components/HelloWorld2.vue'
-  import Vue from 'vue'
 
-  const nodesSelectionMap = {
-  	'trigger': { klass: TriggerOnly },
+  const NODES_SELECTION_MAP = {
+  	'trigger': { 
+		nodeType: 'TriggerOnly', 
+		nodeModule: TriggerOnly, 
+		defaultProps: { 
+			triggerType: "1",
+		}, 
+		defaultOptions: {
+			testOptionHere: "hi there",
+			moreOptComplex: { foo: ['bar', 1234] },
+		},
+	},
   };
   
   
   export default {
+
 	name: 'App',
-	components: {
-	  TriggerOnly,
-	  HelloWorld,
-	  HelloWorld2
+
+	components: { 
+	  //TriggerOnly,
+	  //HelloWorld,
+	  //HelloWorld2
 	},
+
 	data() {
 	  return {
 	    greeting: 'hello world',
@@ -74,39 +93,62 @@
 		selectedNodeType: '',
 	  };
 	},
+
 	mounted() {
 	  // init
 	  const id = document.getElementById("drawflow");
 	  Vue.prototype.$df = new Drawflow(id, Vue, this);
 	  
-	  // the act of registering is a mapping of the String name to the class and default attrs/props
+	  // the act of registering is a mapping of the String name to the class and default props/opts
 	  // this should be done during mounted() time
-	  this.$df.registerNode('TriggerOnly', TriggerOnly, {select_type: "1"}, {});
+	  // see the mapping in the above config
+	  // ---
+	  Object.entries(NODES_SELECTION_MAP).forEach((nsmv) => {
+		const [nk, nconf] = nsmv;
+		// deep copy props and opts, to ensure fresh copy
+		const props = JSON.parse(JSON.stringify(nconf.defaultProps));
+		const opts = JSON.parse(JSON.stringify(nconf.defaultOptions));
+		// .. 
+		this.$df.registerNode(nconf.nodeType, nconf.nodeModule, props, opts);
+		console.log('ok df.registeredNode() ...', nconf.nodeType, nconf.nodeModule, props, opts);
+	  });
 	  this.$df.registerNode('HelloWorld', HelloWorld, {select_type: "2"}, {});
 	  this.$df.registerNode('HelloWorld2', HelloWorld2,{}, {});
 
-	  // required
+	  // required start
 	  this.$df.start();
 
 	  // initial configuration of nodes ... do it here
-	  this.$df.addNode('HelloWorld', 0, 1, 150, 300, 'HelloWorld', {select_type: "1"}, 'HelloWorld', 'vue');
+	  //this.$df.addNode('HelloWorld', 0, 1, 150, 100, 'HelloWorld', {select_type: "1"}, 'HelloWorld', 'vue');
 	},
+
 	methods: {
+
 	  exportDF() {
 		this.exportValue = this.$df.export();
 		console.log(this.exportValue);
 	  },
+
 	  importDF() {
 		this.$df.import(this.exportValue);
 	  },
+
 	  addNodeX() {
-		if (this.$data.selectedNodeType.length <= 0) { return; }
-		console.log('selectedNodeType to add', this.$data.selectedNodeType);
+		const ntypek = this.$data.selectedNodeType;
+		if (ntypek.length <= 0) { return; }
+		if (!(ntypek in NODES_SELECTION_MAP)) { return; }
+		const nconf = NODES_SELECTION_MAP[ntypek];
+		//console.log('selectedNodeType to add:', ntypek, nconf);
+		this.$df.addNode(
+			nconf.nodeType,
+			nconf.nodeModule.methods.INPUTS_COUNT(), 
+			nconf.nodeModule.methods.OUTPUTS_COUNT(), 
+			100, 100, nconf.nodeType, {}, nconf.nodeType, 'vue');
 	  },
 	  addnodedf() {
 		this.$df.addNode('HelloWorld', 0, 1, 150, 300, 'HelloWorld', {select_type: "1"}, 'HelloWorld', 'vue');
 	  },
-	   addnodedf2() {
+	  addnodedf2() {
 		this.$df.addNode('HelloWorld2', 1, 1, 150, 300, 'HelloWorld2', {select_type: "2"}, 'HelloWorld2', 'vue');
 	  }
   
--- src/components/nodes/TriggerOnly.vue
+++ src/components/nodes/TriggerOnly.vue
@@ -1,39 +1,94 @@
 <template>
     <div class="node-trigger-only">
         <h1>Trigger</h1>
-        <select df-select_type @change="onMyChange">
-            <option value="">-- select --</option>
-            <option value="1">on message</option>
-            <option value="2">on mention</option>
-            <option value="3">on conversation expiry </option>
-        </select>
-        <div v-if="select_type">
-            <p>Select type: {{ select_type }}</p>
-        </div>
+        <section class="form">
+            <select df-triggerType v-model="triggerType">
+                <option value="">-- select --</option>
+                <option value="1">on message</option>
+                <option value="2">on mention</option>
+                <option value="3">on conversation expiry </option>
+            </select>
+        </section>
+        <section class="info-block">
+            <span v-if="triggerType > 0">
+                {{ triggerType }}
+            </span>
+        </section>
     </div>
 </template>
 
 <script>
 export default {
     name: 'TriggerOnly',
-    props: {
-        select_type: String,
-    },
     methods: {
-        getConfig: function() {
-            return { inputs: 0, outputs: 1 };
-        },
-        onMyChange: function() {
-            console.log('TriggerOnly onMyChange()', this, this.select_type);
-        },
-    }
+      // note, we've turned these into metadata methods
+      INPUTS_COUNT() { return 0 },
+      OUTPUTS_COUNT() { return 1 },
+    },
+    data() {
+      return {
+        triggerType: "",
+      }  
+    },
+    mounted() {
+      this.$nextTick(() => {
+        const id = this.$el.parentElement.parentElement.id;
+        const data = this.$df.getNodeFromId(id.slice(5));
+        this.triggerType = data.data.triggerType;
+        console.log('TriggerOnly node-id=', id, 'data=', data, this.triggerType);
+      });
+    },
 }
 </script>
 
-<style scoped>
-.node-trigger-only h1 {
-    border: 1px solid red;
-    padding: 5px;
+<style>
+
+/* some node-specific style override here ... */
+
+
+.drawflow .drawflow-node {
+    background: #ebf1f2;
+    padding: 0;
+    width: auto;
+	padding-bottom: 1rem;
 }
+.drawflow .drawflow-node.selected {
+    background: #ebcece;
+}
+
+.drawflow .drawflow-node.selected .node-trigger-only h1 {
+    background: #fcebf8;
+}
+
+.node-trigger-only h1 {
+    margin: 0;
+    padding: 5px;
+    font-size: 1.4rem;
+    background: #ebf6fc;
+    border-bottom: 2px solid #444;
+}
+.node-trigger-only {
+    margin: 0;
+
+    .form {
+        padding: 15px;
+    }
+    .info-block {
+        padding: 15px;
+    }
+
+}
+
+.drawflow .drawflow-node .input, 
+.drawflow .drawflow-node .output {
+    /* e.g. instead, we want this bigger and rounded */
+    width: 25px;
+    height: 25px;
+    border-radius: 20%;
+    border-width: 4px;
+    border-color: #777;
+}
+
+
 
 </style>
